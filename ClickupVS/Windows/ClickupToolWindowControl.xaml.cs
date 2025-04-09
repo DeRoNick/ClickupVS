1ccf8e802d5902ad284ef4c8577678daf5cdbb69
DeRoNick <nikolozkhazhalia1702@gmail.com>
2025-04-09 18:50:48 +0400

    init

---
+++ ClickupVS/Windows/ClickupToolWindowControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClickupVS.Windows
+{
+	/// <summary>
+	/// Interaction logic for ClickupToolWindowControl.
+	/// </summary>
+	public partial class ClickupToolWindowControl : UserControl
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ClickupToolWindowControl"/> class.
+		/// </summary>
+		public ClickupToolWindowControl()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Handles click on the button by displaying a message box.
+		/// </summary>
+		/// <param name="sender">The event sender.</param>
+		/// <param name="e">The event args.</param>
+		[SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+		[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(
+				string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+				"ClickupToolWindow");
+		}
+	}
+}
