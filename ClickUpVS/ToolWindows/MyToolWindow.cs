63334a3a30c3763c9f3a3779ec4b1740b4cebb74
DeRoNick <nikolozkhazhalia1702@gmail.com>
2025-04-09 19:32:01 +0400

    recreate project with community edition

---
+++ ClickUpVS/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClickUpVS
+{
+	public class MyToolWindow : BaseToolWindow<MyToolWindow>
+	{
+		public override string GetTitle(int toolWindowId) => "My Tool Window";
+
+		public override Type PaneType => typeof(Pane);
+
+		public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+		{
+			return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+		}
+
+		[Guid("ac028f23-a72a-49e3-81cd-e64f49d0fe2a")]
+		internal class Pane : ToolkitToolWindowPane
+		{
+			public Pane()
+			{
+				BitmapImageMoniker = KnownMonikers.ToolWindow;
+			}
+		}
+	}
+}
