de2ea3a5c4c1671c8d642a9cd4a6e3ff947ca7fd
DeRoNick <nikolozkhazhalia1702@gmail.com>
2025-04-17 01:56:15 +0400

    add key setting to window

--- ClickUpVS/ToolWindows/ClickUp/ClickUpWindowControl.xaml.cs
+++ ClickUpVS/ToolWindows/ClickUp/ClickUpWindowControl.xaml.cs
@@ -1,18 +1,55 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ClickUpVS
 {
 	public partial class ClickUpWindowControl : UserControl
 	{
+		private General _options;
+
 		public ClickUpWindowControl()
 		{
 			InitializeComponent();
+			_ = InitializeAsync();
+		}
+
+		private async Task InitializeAsync()
+		{
+			_options = await General.GetLiveInstanceAsync();
+
+			if (string.IsNullOrEmpty(_options.ApiKey))
+			{
+				ApiKeyPromptPanel.Visibility = Visibility.Visible;
+				ClickMe.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				ApiKeyPromptPanel.Visibility = Visibility.Collapsed;
+				ClickMe.Visibility = Visibility.Visible;
+			}
 		}
 
 		private void button1_Click(object sender, RoutedEventArgs e)
 		{
 			VS.MessageBox.Show("ClickUpVS", "Button clicked");
+		}
+
+		private async void SaveApiKeyButton_Click(object sender, RoutedEventArgs e)
+		{
+			var key = ApiKeyInput.Text.Trim();
+
+			if (string.IsNullOrEmpty(key))
+			{
+				VS.MessageBox.ShowError("Please enter a valid key", "key cannot be empty");
+				return;
+			}
+
+			_options.ApiKey = key;
+			await _options.SaveAsync();
+
+			ApiKeyPromptPanel.Visibility = Visibility.Collapsed;
+			ClickMe.Visibility = Visibility.Visible;
 		}
 	}
 }
