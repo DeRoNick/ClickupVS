cc32fdfad513ac434e8a9fc5c11d62b69fc3f325
DeRoNick <nikolozkhazhalia1702@gmail.com>
2025-04-18 04:41:46 +0400

    add workspace selector

--- ClickUpVS/ToolWindows/ClickUp/ClickUpWindowControl.xaml.cs
+++ ClickUpVS/ToolWindows/ClickUp/ClickUpWindowControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using ClickUpVS.Services;
+using Microsoft.VisualStudio.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,11 +9,13 @@ namespace ClickUpVS
 	public partial class ClickUpWindowControl : UserControl
 	{
 		private General _options;
+		private ClickupService _service;
 
 		public ClickUpWindowControl()
 		{
 			InitializeComponent();
 			_ = InitializeAsync();
+			WorkspaceSelector.SelectionChanged += OnSelectionChanged;
 		}
 
 		private async Task InitializeAsync()
@@ -21,12 +25,13 @@ namespace ClickUpVS
 			if (string.IsNullOrEmpty(_options.ApiKey))
 			{
 				ApiKeyPromptPanel.Visibility = Visibility.Visible;
-				ClickMe.Visibility = Visibility.Collapsed;
+				MainUIPanel.Visibility = Visibility.Collapsed;
 			}
 			else
 			{
 				ApiKeyPromptPanel.Visibility = Visibility.Collapsed;
-				ClickMe.Visibility = Visibility.Visible;
+				MainUIPanel.Visibility = Visibility.Visible;
+				_service = new ClickupService(_options.ApiKey);
 			}
 		}
 
@@ -49,7 +54,43 @@ namespace ClickUpVS
 			await _options.SaveAsync();
 
 			ApiKeyPromptPanel.Visibility = Visibility.Collapsed;
-			ClickMe.Visibility = Visibility.Visible;
+			MainUIPanel.Visibility = Visibility.Visible;
+			_service = new ClickupService(_options.ApiKey);
+
+			ThreadHelper.JoinableTaskFactory.RunAsync(async () => await LoadDataAsync()).FireAndForget();
+		}
+
+		private async Task LoadDataAsync()
+		{
+			try
+			{
+				var workspaces = await _service.GetWorkSpacesAsync();
+
+				await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+				WorkspaceSelector.WorkspaceComboBox.ItemsSource = workspaces;
+
+				if (workspaces.Count > 0)
+				{
+					WorkspaceSelector.WorkspaceComboBox.SelectedIndex = 0;
+				}
+			}
+			catch (Exception ex)
+			{
+				ex.Log();
+			}
+		}
+
+		private async void ClickUp_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (_service is null) return;
+
+			ThreadHelper.JoinableTaskFactory.RunAsync(async () => await LoadDataAsync()).FireAndForget();
+		}
+
+		private void OnSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			VS.MessageBox.Show("on selection changed");
 		}
 	}
 }
